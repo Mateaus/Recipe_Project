9963f4d199cd9e7f9be1a228a2d914f0ce683947
Mat <mateausc@gmail.com>
2019-02-22 20:27:02 -0600

    Added two extra comments in food app views to easily explain how everything works.

--- school_project/food/views.py
+++ school_project/food/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-# We will start with function Home
-# this will handle the traffic from the
-# home page of our list of foods page
+# This is our home function to handle home requests
 def home(request):
 	return HttpResponse('<h1>Food Recipes</h1>')
 
+# This is an example function to handle /example requests
 def example(request):
 	return HttpResponse('<h1>Example View</h1>')
