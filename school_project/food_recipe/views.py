e08829adfe82b88e1c89366ef70455fbf4f9ac42
Mat <mateausc@gmail.com>
2019-04-07 00:22:51 -0500

    Changed the way the site looks like with some static data before connecting it to a database.

--- school_project/food_recipe/views.py
+++ school_project/food_recipe/views.py
@@ -1,9 +1,78 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+posts = [
+	{
+		'author': 'Joseph C.',
+		'title' : 'Potato Soup',
+		'image' : '/static/images/potato-soup.jpg',
+		'content' : 'Description of potato soup recipy.',
+		'date_posted': 'April 6, 2019'
+	},
+	{
+		'author': 'Anthony Viera',
+		'title' : 'Spaghetti',
+		'image' : '/static/images/spaghetti.jpg',
+		'content' : 'Description of Spaghetti recipy.',
+		'date_posted': 'April 7, 2019'
+	},
+	{
+		'author': 'Charles Garza',
+		'title' : 'Bibimbap',
+		'image' : '/static/images/bibimbap.jpg',
+		'content' : 'Description of Bibimbap recipy.',
+		'date_posted': 'April 8, 2019'
+	},
+	{
+		'author': 'Joseph C.',
+		'title' : 'Potato Soup',
+		'image' : '/static/images/potato-soup.jpg',
+		'content' : 'Description of potato soup recipy.',
+		'date_posted': 'April 6, 2019'
+	},
+	{
+		'author': 'Anthony Viera',
+		'title' : 'Spaghetti',
+		'image' : '/static/images/spaghetti.jpg',
+		'content' : 'Description of Spaghetti recipy.',
+		'date_posted': 'April 7, 2019'
+	},
+	{
+		'author': 'Charles Garza',
+		'title' : 'Bibimbap',
+		'image' : '/static/images/bibimbap.jpg',
+		'content' : 'Description of Bibimbap recipy.',
+		'date_posted': 'April 8, 2019'
+	},
+	{
+		'author': 'Joseph C.',
+		'title' : 'Potato Soup',
+		'image' : '/static/images/potato-soup.jpg',
+		'content' : 'Description of potato soup recipy.',
+		'date_posted': 'April 6, 2019'
+	},
+	{
+		'author': 'Anthony Viera',
+		'title' : 'Spaghetti',
+		'image' : '/static/images/spaghetti.jpg',
+		'content' : 'Description of Spaghetti recipy.',
+		'date_posted': 'April 7, 2019'
+	},
+	{
+		'author': 'Charles Garza',
+		'title' : 'Bibimbap',
+		'image' : '/static/images/bibimbap.jpg',
+		'content' : 'Description of Bibimbap recipy.',
+		'date_posted': 'April 8, 2019'
+	}
+]
+
 # This is our home function to handle home requests
 def home(request):
-	return render(request, 'food_recipe/index.html')
+	context = {
+		'posts': posts
+	}
+	return render(request, 'food_recipe/index.html', context)
 
 # This is an example function to handle /example requests
 def example(request):
